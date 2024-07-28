1a58cb71b4c29cbac7892c0b5762d3fd86f13b8b
AyushiBadika <badikaayushi@gmail.com>
2024-07-29 01:02:13 +0530

    added vercel.json

--- src/components/addNewPerson/AddNewPerson.jsx
+++ src/components/addNewPerson/AddNewPerson.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AddNewPerson() {
   const [age, setAge] = useState(0);
   const [open, setOpen] = useState(false);
-  const [user, setUser] = useState([]);
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [aadhar, setAadhar] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [users, setUsers] = useState("");
 
   const handleAge = (e) => {
     const dateFilled = e.target.value;
@@ -15,12 +19,34 @@ export default function AddNewPerson() {
 
   const handleLocalStorage = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const { name, mobile, aadhar, date, age } = formData;
-    console.log(name, date, aadhar, mobile);
-    // const dataFromLocalStorage = localStorage.getItem("data") || {};
+    if (name != "" && dob != "" && aadhar != "") {
+      const formData = { name, dob, aadhar, mobile, age };
+      const dataFromLocalStorage =
+        JSON.parse(localStorage.getItem("data")) || [];
+
+      dataFromLocalStorage.push(formData);
+
+      localStorage.setItem("data", JSON.stringify(dataFromLocalStorage));
+      fetchDataFromLocalStorage();
+    }
   };
 
+  const handleDelete = (index) => {
+    users.splice(index, 1);
+    localStorage.setItem("data", JSON.stringify(users));
+    setUsers(JSON.parse(localStorage.getItem("data")));
+    fetchDataFromLocalStorage();
+  };
+
+  useEffect(() => {
+    fetchDataFromLocalStorage();
+  }, [name, dob, aadhar, mobile, age]);
+
+  function fetchDataFromLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+    console.log(data);
+    setUsers(data);
+  }
   return (
     <div className="mx-8 border p-8 h-[70vh]  relative">
       <div>
@@ -28,34 +54,82 @@ export default function AddNewPerson() {
           Add New Person
         </h2>
         {/* table */}
+        {users.length > 0 && (
+          <table className="mt-12 w-full ">
+            <thead className=" bg-blue-500 text-white ">
+              <th className="p-4">Name</th>
+              <th className="p-4">Date of Birth</th>
+              <th className="p-4">Aadhar Number</th>
+              <th className="p-4">Mobile Number</th>
+              <th className="p-4">Age</th>
+              <th className="p-4">Actions</th>
+            </thead>
+            <tbody className="border border-collapse">
+              {users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="border p-3 text-center">{user.name}</td>
+                    <td className="border p-3  text-center">{user.dob}</td>
+                    <td className="border p-3  text-center">{user.aadhar}</td>
+                    <td className="border p-3  text-center">{user.mobile}</td>
+                    <td className="border p-3  text-center">{user.age}</td>
+                    <td
+                      className="border p-3  text-center cursor-pointer "
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
         {/* form */}
         {open && (
-          <div className="mt-12 flex flex-col justify-center items-center p-8 bg-blue-500 rounded text-white">
+          <div className="mt-12 flex flex-col justify-center items-center p-4 bg-blue-500 rounded text-white">
             <h3 className="text-lg mb-4">Fill Below form for New Entry</h3>
-            <form action="" className="flex w-full ">
+            <form onSubmit={handleLocalStorage} className="flex w-full ">
               <input
                 type="text"
                 placeholder="Name"
                 className="border py-2 px-4  text-black grow"
                 name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
               />
               <input
                 type="date"
-                onChange={handleAge}
+                // onChange={handleAge}
                 className="border py-2 px-4  text-black grow"
                 name="date"
+                value={dob}
+                onChange={(e) => {
+                  setDob(e.target.value);
+                  handleAge(e);
+                }}
+                required
               />
               <input
                 type="number"
                 placeholder="Aadhar Number"
                 className="border py-2 px-4  text-black grow"
                 name="aadhar "
+                value={aadhar}
+                onChange={(e) => setAadhar(e.target.value)}
+                required
+                minLength={12}
               />
               <input
                 type="number"
                 placeholder="Mobile Number"
                 className="border py-2 px-4  text-black grow"
-                name="mobile "
+                name="mobile"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+                required="true"
+                minLength={10}
               />
               <input
                 className="border py-2 px-4  text-black grow bg-white "
@@ -64,8 +138,8 @@ export default function AddNewPerson() {
               />
 
               <button
+                type="submit"
                 className="border py-2 px-4  text-black grow bg-white"
-                onClick={(e) => handleLocalStorage(e)}
               >
                 Save
               </button>
